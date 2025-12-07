676c3cac7857fb77efb92adfc77027c251d66c34
m- <m@nonzerosum.xyz>
2025-12-07 17:28:39 +1100

    feat: moves python lsp to pyright for better hints

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- the following are modifications from the LazyVim kickstart
 --
+-- set python lsp to basedpyright
+vim.g.lazyvim_python_lsp = "basedpyright"
+-- set soft wrap
 vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
